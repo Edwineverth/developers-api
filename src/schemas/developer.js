d5ea8772cbed1cf55f807afd255dc5380cf2883d
Edwin Belduma <edwineverth94@gmail.com>
2020-05-25 17:12:59 -0500

    Create Schema Developer for validation

---
+++ src/schemas/developer.js
@@ -0,0 +1,24 @@
+import joi from "@hapi/joi";
+
+const developerIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const developerFullName = joi.string().max(200);
+const developerLinkGithub = joi.string().uri();
+const developerTechnology = joi.array().items(joi.string().max(50));
+
+const createDeveloperSchema = {
+  nombres_completos: developerFullName.required(),
+  link_github: developerLinkGithub.required(),
+  tecnologias_conocidas: developerTechnology.required(),
+};
+
+const updateDeveloperSchema = {
+  nombres_completos: developerFullName.required(),
+  link_github: developerLinkGithub.required(),
+  tecnologias_conocidas: developerTechnology.required(),
+};
+
+module.exports = {
+  createDeveloperSchema,
+  updateDeveloperSchema,
+  developerIdSchema,
+};
